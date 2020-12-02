87eb81db6b0dc9603f59f4c6096683f94226d619
Talebian12 <nicco199os@gmail.com>
2020-12-02 12:41:06 +0100

    Changes

--- js/menu.js
+++ js/menu.js
@@ -1,11 +1,20 @@
 const menuBtn = document.querySelector('.menu-btn');
+const menuPage = document.querySelector('#menu');
+const menuList = document.querySelector('#menu-list');
+
+
+
 let menuOpen = false;
 menuBtn.addEventListener('click', () => {
     if(!menuOpen) {
         menuBtn.classList.add('open');
+        menuPage.classList.remove('open');
+        menuList.classList.remove('open');
         menuOpen = true;
     } else {
         menuBtn.classList.remove('open');
+        menuPage.classList.add('open');
+        menuList.classList.add('open');
         menuOpen = false;
     }
 });
